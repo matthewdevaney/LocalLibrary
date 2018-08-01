dffec36b08e6ae07c6116b29171dfe9edbe15ca2
mdevaney <mbdevaney@gmail.com>
2018-07-31 19:58:37 -0500

    Part 6: Generic list and detail views

--- catalog/views.py
+++ catalog/views.py
@@ -11,6 +11,16 @@ class BookListView(generic.ListView):
     model = Book
     paginate_by = 10
 
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+
 # Create your views here.
 def index(request):
     """View function for home page of site"""
@@ -25,11 +35,16 @@ def index(request):
     # The 'all()' is implled by default
     num_authors = Author.objects.count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
